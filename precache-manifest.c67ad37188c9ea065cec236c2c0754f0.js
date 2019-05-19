54583c2a2079f8a0f0e1bf2017821666423f3b0c
yogesh mishra <yogesh.mishra@habilelabs.io>
2019-05-19 20:51:57 +0530

    Updates

---
+++ precache-manifest.c67ad37188c9ea065cec236c2c0754f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102cbc4b2363a2a7afa34ee363bfc0b8",
+    "url": "/react-demo-app/index.html"
+  },
+  {
+    "revision": "2e602a2406d4a73c6be2",
+    "url": "/react-demo-app/static/css/main.c253adb1.chunk.css"
+  },
+  {
+    "revision": "42a18de2ab93955d9ef4",
+    "url": "/react-demo-app/static/js/2.daa88a29.chunk.js"
+  },
+  {
+    "revision": "2e602a2406d4a73c6be2",
+    "url": "/react-demo-app/static/js/main.f23c2917.chunk.js"
+  },
+  {
+    "revision": "89b55dca5aaa7e551c9c",
+    "url": "/react-demo-app/static/js/runtime~main.5d1603a4.js"
+  },
+  {
+    "revision": "4fb962b63886f5980334e662973262e1",
+    "url": "/react-demo-app/static/media/background.4fb962b6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
